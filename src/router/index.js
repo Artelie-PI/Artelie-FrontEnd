099cc7231cd8d8e2752f67b692f3f3258b8de9ed
gabrielmartinsgomes <gabrielhenrickmartind3454>
2025-09-18 23:06:05 -0300

    fix: conflito no pr

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import DefaultLayout from '@/layouts/DefaultLayout.vue'
 import ProductLayout from '@/layouts/ProductLayout.vue'  // <-- aqui
 import ProductView from '@/views/ProductView.vue'
 import RelatedProduct from '@/components/RelatedProduct.vue'
-
+import CategoryView from '@/views/CategoryView.vue' // import direto; você pode lazy-load se preferir
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -14,11 +14,8 @@ const router = createRouter({
       name: 'default',
       component: DefaultLayout,
       children: [
-        {
-          path: 'home',
-          name: 'home',
-          component: HomeView,
-        },
+       { path: '', name: 'home', component: HomeView },
+       { path: 'category/:slug', name: 'category', component: CategoryView, props: true },
       ],
     },
     {
